27e77a9a1583e766ab0fda7aea12b1ee8c60e316
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-09-20 12:56:50 +0700

    #code

--- 01_pytorch_workflow/main.py
+++ 01_pytorch_workflow/main.py
@@ -1,22 +1,52 @@
 from LinearRegression import LinearRegression
 import torch.nn as nn
 import torch
-from preparing_loading_data import X_test
+import matplotlib.pyplot as plt
+from preparing_loading_data import X_test, X_train, y_train, y_test
+import numpy as np
 
-torch.manual_seed(42)
-
+# torch.manual_seed(42)
 model = LinearRegression()
-
 print((model.state_dict()))
-
-with torch.inference_mode():
-    preds = model(X_test)
-
-print(preds)
-
-
 print("Set up loss and optim")
 loss_fn = nn.L1Loss()
 optimizer = torch.optim.SGD(params=model.parameters(),
                             lr=0.01) # optim
+# Training
+epoch_count = []
+loss_values = []
+test_loss_values = []
+epochs = 100 # one loop through the data - hyperparameter
+for epoch in range(epochs):
+    # training mode
+    model.train()
+    # forward pass
+    y_pred = model(X_train)
+    # loss func
+    loss = loss_fn(y_pred, y_train)
+    print(f"Loss: {loss}")
+    # Optimizer zero grad
+    optimizer.zero_grad()
+    # BP
+    loss.backward()
+    # step optimizer
+    optimizer.step()
 
+    # eval mode test set up?
+    model.eval()
+    with torch.inference_mode(): # turn off grad tracking
+        test_pred = model(X_test)
+        test_loss = loss_fn(test_pred, y_test)
+    if epoch % 10 == 0:
+        epoch_count.append(epoch)
+        loss_values.append(loss)
+        test_loss_values.append(test_loss)
+        print(f"Epoch: {epoch} | Loss: {loss} | Test loss: {test_loss}")
+        print(model.state_dict())
+
+plt.plot(epoch_count, np.array(torch.tensor(loss_values).numpy()), label="Train loss")
+plt.plot(epoch_count, test_loss_values, label="Test loss")
+plt.ylabel("Loss")
+plt.xlabel("Epochs")
+plt.legend()
+plt.show()
--- 01_pytorch_workflow/preparing_loading_data.py
+++ 01_pytorch_workflow/preparing_loading_data.py
@@ -1,5 +1,4 @@
 import torch
-import matplotlib.pyplot as plt
 
 weight = 0.1
 bias = 0.3
@@ -21,25 +20,3 @@ X_train, y_train = X[:train_split], y[:train_split]
 X_test, y_test = X[train_split:], y[train_split:]
 print("train", len(X_train), len(y_train), "test", len(X_test), len(y_test))
 
-
-# Visualize data
-def plot_predictions(train_data=X_train,
-                     train_labels=y_train,
-                     test_data=X_test,
-                     test_labels=y_test,
-                     predictions=None):
-    """
-    Plots training data, test data and compares predictions
-    """
-    plt.figure(figsize=(10, 7))
-    plt.scatter(train_data, train_labels, c="b", s=4, label="training data")
-
-    plt.scatter(test_data, test_labels, c="r", s=4, label="testing data")
-
-    if predictions is not None:
-        plt.scatter(test_data, predictions, c='r', s='4', label='Predictions')
-
-    plt.legend(prop={"size": 14})
-
-
-plot_predictions()
