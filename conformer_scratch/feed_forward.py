d776b7fb5bec214af3a718042a794effc23a5a41
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-01-30 13:57:07 +0700

    #code

--- conformer_scratch/feed_forward.py
+++ conformer_scratch/feed_forward.py
@@ -4,14 +4,29 @@ from conformer_scratch.activations import Swish
 
 
 class ConformerFF(nn.Module):
-    def __init__(self):
-        self.norm_layer = nn.LayerNorm()
-        self.sub_linear1 = nn.Linear()  # first Linear layer
+    def __init__(self, input_size: int, output_size: int):
+        """
+        :param input_size: number of weight after dropout
+        This FF network consists of LayerNorm -> Linear -> Dropout -> Linear -> Swish
+        """
+
+        self.norm_layer = nn.LayerNorm() # config LayerNorm
+
+        # config in feats and out feats of sub-linear 1 network
+        self.sub_linear1 = nn.Linear(in_features=input_size,
+                                     out_features=100, bias=True)
+
+        # config dropout for common usage in FF block
         self.dropout = nn.Dropout(p=0.1)  # common dropout
-        self.sub_linear2 = nn.Linear()  # final Linear layer
+
+        # config in feats and out feats of sub-linear 2 network
+        self.sub_linear2 = nn.Linear(in_features=100, out_features=10,
+                                     bias=True)  # final Linear layer
+
+        # customize Swish activation function
         self.swish = Swish()
 
-        # Sequential for feed forward
+        # combine all these block to form a sequence FF
         self.linear = nn.Sequential(
             self.norm_layer,
             self.sub_linear1,
@@ -22,6 +37,6 @@ class ConformerFF(nn.Module):
         )
 
     def forward(self, x):
-        """ input is weights from Linear layer """
+        """ input is weights from Linear layer after Dropout """
         x = self.linear(x)
         return x  # return output of FF network
